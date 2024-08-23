f0651a44bf661998b52cf4b6a09594615188bf59
Pratham-Techops <poddarpratham@gmail.com>
2024-08-23 13:19:49 +0530

    added url

--- client/src/socekt.ts
+++ client/src/socekt.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chess-game-amber-omega.vercel.app");
+const socket = io("https://chess-game-amber-omega.vercel.app/");
 
 export default socket;
