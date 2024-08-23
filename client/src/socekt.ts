5201584fee5265687c82faddaa3459704417ae25
Pratham-Techops <poddarpratham@gmail.com>
2024-08-23 14:16:32 +0530

    add sockets

--- client/src/socekt.ts
+++ client/src/socekt.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chess-game-amber-omega.vercel.app/", {
+const socket = io("https://chess-game-backend-oxbc.onrender.com/", {
   transports: ["websocket"],
 });
 
