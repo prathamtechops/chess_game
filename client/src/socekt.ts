6b8c5cc3a5f8848fb90f9e0103769cad0ad8548a
Pratham-Techops <poddarpratham@gmail.com>
2024-09-03 20:07:42 +0530

    change backend link

--- client/src/socekt.ts
+++ client/src/socekt.ts
@@ -1,8 +1,15 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://chess-game-backend-oxbc.onrender.com/", {
-  transports: ["websocket"],
-});
+// const socket = io("https://chess-game-backend-oxbc.onrender.com/", {
+//   transports: ["websocket"],
+// });
+
+const socket = io(
+  "https://convosechessgame-ghbac4hhdud9bcd7.eastus-01.azurewebsites.net/",
+  {
+    transports: ["websocket"],
+  }
+);
 
 // const socket = io("http://localhost:3000/", {
 //   transports: ["websocket"],
