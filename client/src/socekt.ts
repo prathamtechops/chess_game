aeac4583a68bb711fc5d441845a937eb956b39c3
prathamtechops <poddarpratham@gmail.com>
2024-09-21 20:16:03 +0530

    updated backend

--- client/src/socekt.ts
+++ client/src/socekt.ts
@@ -1,14 +1,14 @@
 import { io } from "socket.io-client";
 
-// const socket = io(
-//   "https://convosechessgame-ghbac4hhdud9bcd7.eastus-01.azurewebsites.net/",
-//   {
-//     transports: ["websocket"],
-//   }
-// );
+const socket = io(
+  "https://convosechessgame-ghbac4hhdud9bcd7.eastus-01.azurewebsites.net/",
+  {
+    transports: ["websocket"],
+  }
+);
 
-const socket = io("http://localhost:8000/", {
-  transports: ["websocket"],
-});
+// const socket = io("http://localhost:8000/", {
+//   transports: ["websocket"],
+// });
 
 export default socket;
